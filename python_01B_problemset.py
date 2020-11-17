214c7ecbc9bdf211ace6015498212addd8d902b1
Diane Downhour <diane@Nels-Eldes-iMac.local>
2020-11-17 09:25:29 -0700

    python_01B_problemset.py

---
+++ python_01B_problemset.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import sys
+
+#Python 1 problem set
+#This is part B, where I'm attempting to do this a different way
+
+my_name = sys.argv[1]
+
+print ("my name is", my_name)
+
+color = sys.argv[2]
+
+print ("my favorite color is", color)
+
+activity = sys.argv[3]
+
+print ("my favorite activity is", activity)
+
+animal = sys.argv[4]
+
+print ("my favorite animal is", animal)
+
+#remember, to run this script you type the name of the script AND you must pass the arguments Diane green biking Oliver
+
+
