0e1d6cb1e3c0b582b0749a7f41c073703992c2e4
Diane Downhour <diane@Nels-Eldes-iMac.local>
2020-11-17 09:04:06 -0700

    python_01_problemset.py

---
+++ python_01_problemset.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+
+#Print out Hello New York
+print ("Hello New York")
+
+# store my name in a variable
+my_name = "Diane"
+print ("My name is", my_name)
+
+#add code to print out my favorite color
+my_favorite_color = "green"
+print ("My favorite color is", my_favorite_color)
+
+#add variable for fav activity and print that out
+my_favorite_activity = "biking"
+print ("My favorite activity is", my_favorite_activity)
+
+#add code and print out favorite animal
+my_favorite_animal = "Oliver"
+print ("My favorite animal is", my_favorite_animal)
+
+import sys
+my_name = sys.argv[1]
+my_favorite_color = sys.argv[2]
+my_favorite_activity = sys.argv[3]
+my_favorite_animal = sys.argv[4]
+
+print ("my name is", my_name)
+print ("my favorite color", my_favorite_color)
+print ("my favorite activity", my_favorite_activity)
+print ("my favorite animal", my_favorite_animal)
+
+
+
