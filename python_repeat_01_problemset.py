7589819679e30c1c11f32ccca882da91946b5549
Diane Downhour <diane@Nels-Eldes-iMac.local>
2021-03-17 08:15:54 -0600

    python_repeat_01_problemset.py

---
+++ python_repeat_01_problemset.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import sys
+
+#remember to add any required modules here
+#can type anything after hash
+
+print('I am reviewing my Python class')
+print('Hello sunshine!')
+print('My name: Diane')
+print('My favorite color: blue')
+print('My favorite activity: biking')
+print('My favorite animal: Oliver')
+
+friend1=sys.argv[1]
+friend2=sys.argv[2]
+print(friend1, 'and', friend2, 'are friends')
+
+myname=sys.argv[3]
+mycolor=sys.argv[4]
+myactivity=sys.argv[5]
+mydog=sys.argv[6]
+
+print('My name is', myname, 'and my favorite color is', mycolor)
+print('my favorite activity is', myactivity, 'and the name of my dog is', mydog)
+
+
+
+
+ 
